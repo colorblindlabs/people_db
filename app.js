8598aef5d23602734fcaf6faf507312c3daf99a3
bayuhafiz <bayu@colorblindlabs.com>
2016-09-15 12:36:11 +0700

    convert .ejs to .html

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ mongoose.connect('mongodb://colorblindlabs:Hello123#@ds019836.mlab.com:19836/sim
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ngPeopleDbApp' });
+    res.send({
+        status: 'success'
+    });
 });
 
 module.exports = router;
